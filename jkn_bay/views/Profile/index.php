28d0e0bd9578f167ba8e92139de901df7dcdacb1
Kyle2411 <82056454+Kyle2411@users.noreply.github.com>
2022-11-06 21:36:39 -0500

    Update index.php

--- jkn_bay/views/Profile/index.php
+++ jkn_bay/views/Profile/index.php
@@ -1,8 +1,21 @@
 <!doctype html>
 <html lang="en">
-	
+	<link rel="stylesheet" href="/style.css"/>
 	<head>
-
+<?php
+			if(isset($_GET['error'])){ ?>
+				<div class="alert alert-danger alert-dismissible">
+  					<a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+  					<?= $_GET['error'] ?>
+				</div>
+		<?php  }
+			if(isset($_GET['message'])){ ?>
+				<div class="alert alert-success alert-dismissible">
+  					<a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+  					<?= $_GET['message'] ?>
+				</div>
+		<?php  }
+		?>
 		<!-- Jquery -->
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 
@@ -17,39 +30,44 @@
 		crossorigin="anonymous"></script>
 		<title>Log in Page</title>
 
-		
+		<div class="navbar">
+  <a class="active" href="#">Home</a> 
+  <a href="#">Search</a> 
+  <img src="/jknimage.png" alt="JKN" />
+  <a href="#">About</a> 
+  <a href="#">Profile</a> 
+</div>
 	</head>
 	
 	<body>
-		<?php
-			if(isset($_GET['error'])){ ?>
-				<div class="alert alert-danger alert-dismissible">
-  					<a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
-  					<?= $_GET['error'] ?>
-				</div>
-		<?php  }
-			if(isset($_GET['message'])){ ?>
-				<div class="alert alert-success alert-dismissible">
-  					<a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
-  					<?= $_GET['message'] ?>
-				</div>
-		<?php  }
-		?>
+		
 
 		<div>
-			<h1>Welcome to JKN-Bay</h1>
-		</div>
+			<div class="description">
+  				<h1 class="title" >Welcome to JKN-Bay</h1>
+  				<p>
+  					The company, which caters to individual sellers and small businesses, is a market leader in e-commerce worldwide. eBay is headquartered in San Jose, California. Customers can participate in Web sites set up within their own country or use one of the company's international sites.
+  					<br></br>
+  					The company, which caters to individual sellers and small businesses, is a market leader in e-commerce worldwide. eBay is headquartered in San Jose, California. Customers can participate in Web sites set up within their own country or use one of the company's international sites.
+  				</p>
+  			</div>
 
+			<h1>Log-In</h1>
+		
+<div class ="line">
+  			</div>
 		<form action='' method='post'>
 			<div class="form-group1">
     			<label for="username">Username</label>
     			<input type="text" class="form-control" id="username" name='username' placeholder="Enter username">
-  			</div><br>
+  			</div>
 
   			<div class="form-group2">
     			<label for="password">Password</label>
     			<input type="password" class="form-control" id="password" name="password" placeholder="Enter Password">
-  			</div><br>
+  			</div>
+
+  			<br>
   			<br>
   			<button type="submit" name='action' value='Login' class="btn btn-primary">Login</button>
   			<br>
@@ -57,5 +75,6 @@
 
 		<p>Don't have an Account? <a href ="/Profile/register">Sign Up</a></p>
 	</body>
-
+</div>
+</html>
 </html>
