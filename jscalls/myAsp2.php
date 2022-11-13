039c65a0927ae857f389849a6229be6558156f26
Kyle2411 <82056454+Kyle2411@users.noreply.github.com>
2022-11-13 15:37:14 -0500

    Add files via upload

---
+++ jscalls/myAsp2.php
@@ -0,0 +1,23 @@
+<?php
+
+$q = $_REQUEST["q"];
+
+
+$out = substr(strstr($q, 'S'), strlen('S'));
+
+$test3 = strstr($out, 'O', true);
+
+$test2 = substr(strstr($q, 'O'), strlen('O'));
+
+$test = strstr($q, ' S', true);
+
+
+$test3++;
+
+echo "
+
+<a href ='/Product/addRating/$test/$test3/$test2' class=' btn btn-success btnSave' name='upvote'> Save
+ </a>
+";
+
+?>
