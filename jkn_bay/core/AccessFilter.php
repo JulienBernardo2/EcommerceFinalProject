075ec234e95d4197cc5c9a5aa8403ef7aa2dd19f
Julien <82057989+JulienBernardo2@users.noreply.github.com>
2022-12-08 10:26:22 -0500

    Delete jkn_bay directory

--- jkn_bay/core/AccessFilter.php
+++
@@ -1,6 +0,0 @@
-<?php
-namespace jkn_bay\core;
-
-abstract class AccessFilter{
-	abstract public function execute();
-}
--- jkn_bay/core/Validator.php
+++
@@ -1,6 +0,0 @@
-<?php
-namespace jkn_bay\core;
-
-abstract class Validator{
-	abstract function isValidData($data);
-}
